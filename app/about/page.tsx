09b8a1228c7085531437375e1f102ce4fdd6e0e3
mohammedayaan1610 <mohammedayaan1610@gmail.com>
2026-08-26 12:04:54 +0530

    first-commit

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>abouit page</div>
+  )
+}
+
+
+
+export default page
---
+++ app/projects/[slug]/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import React, { useState } from 'react'
+
+function page() {
+    const [first, setfirst] = useState(0)
+  return (
+    <div>project details page</div>
+  )
+}
+
+export default page
